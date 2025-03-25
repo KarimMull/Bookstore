0d5ce73e19a8c8fcd60f0d7803863d4da28cfa7b
Karim <kmullayanov@duck.com>
2025-03-25 23:39:59 +0300

    Добавлен ajax

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -1,12 +1,12 @@
 from django import template
+
 from carts.models import Cart
+from carts.utils import get_user_carts
+
 
 register = template.Library()
 
 
 @register.simple_tag()
 def user_carts(request):
-    user = getattr(request, "user", None)
-    if hasattr(user, "is_authenticated") and user.is_authenticated:
-        return Cart.objects.filter(user=user)
-    return Cart.objects.none()  # безопасно вернуть пустой QuerySet
+    return get_user_carts(request)
