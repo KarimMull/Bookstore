09d58c1e20da4c52287e0745daee5ab549e8c8a6
Karim <kmullayanov@duck.com>
2025-03-26 21:02:56 +0300

    Добавлено кэширование и еще много чего

--- goods/urls.py
+++ goods/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+
+from goods import views
 
 app_name = 'goods'
 
 urlpatterns = [
-    path('search/', views.catalog, name='search'),
-    path('<slug:category_slug>/', views.catalog, name='index'),
-    path('product/<slug:product_slug>/', views.product, name='product'),
+    path('search/', views.CatalogView.as_view(), name='search'),
+    path('<slug:category_slug>/', views.CatalogView.as_view(), name='index'),
+    path('product/<slug:product_slug>/', views.ProductView.as_view(), name='product'),
 ]
+
