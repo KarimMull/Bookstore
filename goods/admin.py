5a621b633acc4d47556b0e1919c01993d414cb56
Karim <kmullayanov@duck.com>
2025-04-12 20:29:03 +0300

    Сделано показ нескольких изображений товара

--- goods/admin.py
+++ goods/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-
 from goods.models import Categories, Products
-
-# admin.site.register(Categories)
-# admin.site.register(Products)
 
 
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ["name",]
+
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
@@ -18,12 +15,15 @@ class ProductsAdmin(admin.ModelAdmin):
     list_editable = ["discount",]
     search_fields = ["name", "description"]
     list_filter = ["discount", "quantity", "category"]
+
     fields = [
         "name",
         "category",
         "slug",
         "description",
         "image",
+        "image_2",  # Добавлено поле для второго изображения
+        "image_3",  # Добавлено поле для третьего изображения
         ("price", "discount"),
         "quantity",
     ]
